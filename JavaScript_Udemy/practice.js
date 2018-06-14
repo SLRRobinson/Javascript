80419bda687b96c0aa2820c464ba0b679641b5f7
SLRRobinson <infos@gscs.ch>
2018-06-14 19:03:38 +0200

    Add files via upload

---
+++ JavaScript_Udemy/practice.js
@@ -0,0 +1,21 @@
+
+let myName=document.getElementById("names").innerHTML="Hello David";
+
+
+
+
+
+
+function callMe(name,vorname){
+    var name=document.getElementById("name").value;
+    if(name=='Peter'){
+        console.log('African');
+     } else{
+         console.log('European');
+
+        }
+    }
+
+
+console.log(callMe);
+
