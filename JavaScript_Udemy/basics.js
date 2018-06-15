6e0f8734d10581dc9c8c51912b50882d20306c4a
SLRRobinson <infos@gscs.ch>
2018-06-15 19:05:40 +0200

    Add files via upload

---
+++ JavaScript_Udemy/basics.js
@@ -0,0 +1,105 @@
+/*
+function validateForm() {
+    let vorname = document.forms["checkForm"]["vorname"].value;
+    let nachname= document.forms["checkForm"]["nachname"].value;
+    if (vorname == "") {
+        alert("Vorname  muss eingegeben werden bitte.");
+        return false;
+    }
+
+    if(nachname == ""){
+        alert(" Nachname muss eingegeben werden bitte.");
+        return false;
+    }
+}
+
+let headers=document.getElementById('header').innerHTML='Contact.name.value';
+console.log('headers');
+
+document.write('console.log(headers)');
+
+function caller(name, vorname){
+    return name +"" + vorname
+}
+
+caller('david','Aaron');
+
+console.log(headers)
+document.write(5+4);
+
+
+
+var person = prompt("Please enter your name", "Harry Potter");
+console.log(person);
+
+
+// Create Object using Constructor function.
+function Person(firstName,lastName){
+    this.firstName=firstName;
+    this.lastName=lastName;
+
+}
+
+const person1= new Person('David','Robinson');
+console.log(person1);
+
+function caller(){
+    console.log(person1);
+}
+
+// Set balance and price of item
+const balance = 500;
+const jeans = 40;
+
+// Check if there are enough funds to purchase item
+if (jeans <= balance) {
+  console.log("You have enough money to purchase the item!");
+}
+
+
+function colorChecker(myColor){
+    if(myColor=='Blue'){
+
+        alert('African');
+    }else{
+        alert("Alien");
+    }    
+
+}
+*/
+
+function callMe(name,vorname){
+    var name=document.getElementById("name").value;
+    if(name=='Peter'){
+        console.log('African');
+     } else{
+         console.log('European');
+
+        }
+    }
+
+
+console.log(callMe);
+
+
+
+function teller(name,age){
+    var name=document.getElementById('name').value;  
+    var age=document.getElementById('ages').value;
+    if((age==12)&&(name="David")){
+        console.log("You are young" + " "+ name);
+    }
+else{
+    if(age >=14){
+    console.log("you are a teeneger" + " " + name);
+    }
+}
+}
+
+  /**    
+var age=document.forms['myForm'] [age];
+var name = document.forms["myForm"]["Name"]; 
+**/
+        
+    
+    
